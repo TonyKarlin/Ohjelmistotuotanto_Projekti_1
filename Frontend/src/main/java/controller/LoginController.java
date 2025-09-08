ac186f70bc9fcf74aae0d24451e798527c660863
jonijermu <joni.heikkila@metropolia.fi>
2025-09-08 14:33:25 +0300

    Add login and registration views with skeleton controllers and styles

---
+++ Frontend/src/main/java/controller/LoginController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private TextField passwordTextfield;
+
+    @FXML
+    private Label signUpLabel;
+
+    @FXML
+    private TextField userNameTextfield;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signUp(MouseEvent event) {
+
+    }
+
+}
+
