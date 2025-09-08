fa8d864416ccce0cd299831b1a5b8b3fae02ac46
onnik <onni.kivinen@gmail.com>
2025-09-08 16:35:41 +0300

    updated database

--- Backend/database-creation.sql
+++ Backend/database-creation.sql
@@ -10,6 +10,8 @@ CREATE TABLE IF NOT EXISTS users
 (
     id         BIGSERIAL PRIMARY KEY,
     username   VARCHAR(255) NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    email      VARCHAR(255) NOT NULL,
     created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
