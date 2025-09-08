43669e444c3e958b721ec4839223665b935727af
onnik <onni.kivinen@gmail.com>
2025-09-08 17:54:02 +0300

    added jsonIgnore to User.conversation

--- Backend/src/main/java/backend_api/entities/User.java
+++ Backend/src/main/java/backend_api/entities/User.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -34,6 +36,7 @@ public class User {
     private LocalDateTime createdAt;
 
     @ManyToMany(mappedBy = "participants")
+    @JsonIgnore
     private List<Conversation> conversations = new ArrayList<>();
 
     public User() {
