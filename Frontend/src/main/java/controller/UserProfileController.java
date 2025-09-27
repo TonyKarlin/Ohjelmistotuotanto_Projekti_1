7516cc769a35e8a0c38e78eb6083caa888f74a3c
jonijermu <joni.heikkila@metropolia.fi>
2025-09-28 00:35:23 +0300

    checkers for userProfileController

--- Frontend/src/main/java/controller/UserProfileController.java
+++ Frontend/src/main/java/controller/UserProfileController.java
@@ -14,15 +14,12 @@ import request.UpdateUserRequest;
 import service.UserApiClient;
 import utils.UIAlert;
 
-import java.util.Objects;
-
-
 public class UserProfileController {
 
     User loggedInUser;
     UserApiClient client;
     ChatDashboardController parentController;
-    UIAlert uiAlert = new UIAlert();
+    UIAlert alert = new UIAlert();
 
 
     public void setController(User loggedInUser, UserApiClient client, ChatDashboardController parentController) {
@@ -69,15 +66,29 @@ public class UserProfileController {
     private TextField usernameTextField;
 
     @FXML
+    public void changeUserStatus(MouseEvent event) {
+        Circle clickedCircle = (Circle) event.getSource();
+        userCurrentStatus.setFill(clickedCircle.getFill());
+    }
+
+    @FXML
     public void changeInformation(ActionEvent event) {
-        System.out.println(loggedInUser.getToken());
-        System.out.println(loggedInUser.getId());
         String newUsername = usernameTextField.getText();
         String newEmail = emailTextField.getText();
         String newPassword = passwordField.getText();
+        String repeatedPassword = repeatPasswordField.getText();
+        if (!checkTextFields(newUsername, newEmail)) {
+            return;
+        }
+        if (!checkPassword(newPassword, repeatedPassword)) {
+            return;
+        }
         UpdateUserRequest request = new UpdateUserRequest(newUsername, newEmail, newPassword);
         User updatedUser = client.updateUser(request, loggedInUser);
-        if(updatedUser != null) {
+        if (updatedUser == null) {
+            alert.showErrorAlert("Updated failed", "Failed to update user");
+        } else {
+            alert.showSuccessAlert("Success", "User updated successfully ✅");
             String token = loggedInUser.getToken();
             loggedInUser = updatedUser;
             loggedInUser.setToken(token);
@@ -88,11 +99,34 @@ public class UserProfileController {
         }
     }
 
-    @FXML
-    public void changeUserStatus(MouseEvent event) {
-        Circle clickedCircle = (Circle) event.getSource();
-        userCurrentStatus.setFill(clickedCircle.getFill());
+    public boolean checkPassword(String password, String repeatedPassword) {
+        if (password.isEmpty() && repeatedPassword.isEmpty()) {
+            return true;
+        }
+        if (!password.equals(repeatedPassword)) {
+            alert.showErrorAlert("Error", "passwords don't match");
+            return false;
+        }
+        if (password.length() < 6) {
+            alert.showErrorAlert("Invalid Password", "Password should contain 6 or more characters.");
+            return false;
+        }
+        return true;
+    }
 
+    // Check the user inputs that hey are valid. Gets alert if something is wrong
+    public boolean checkTextFields(String username, String email) {
+        if (username.isEmpty() || email.isEmpty()) {
+            alert.showErrorAlert("Empty fields", "Please fill in all fields");
+            return false;
+        } else if (username.length() < 6) {
+            alert.showErrorAlert("Invalid Name", "Name should contain 6 or more characters.");
+            return false;
+        } else if (!email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
+            alert.showErrorAlert("Invalid Email", "Please enter a valid email address.");
+            return false;
+        }
+        return true;
     }
 
     public void addUserInformation(User loggedInUser) {
