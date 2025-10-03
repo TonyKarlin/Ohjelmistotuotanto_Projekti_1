778416ff1820e59fc02a06e26659bacad10b7e71
onnik <onni.kivinen@gmail.com>
2025-10-03 17:11:20 +0300

    Update API_URL to use environment variable for backend URL

--- Frontend/src/main/java/utils/ApiUrl.java
+++ Frontend/src/main/java/utils/ApiUrl.java
@@ -2,7 +2,9 @@ package utils;
 
 public class ApiUrl {
 
-    private final static String API_URL = "http://localhost:8081/api";
+    private final static String API_URL = System.getenv("BACKEND_URL") != null
+            ? System.getenv("BACKEND_URL") + "/api"
+            : "http://localhost:8081/api";
 
     public static String getApiUrl() {
         return API_URL;
