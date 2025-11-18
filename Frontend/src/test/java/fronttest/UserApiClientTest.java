2688d842b030feae143de6af50b7af648116573d
onnik <onni.kivinen@gmail.com>
2025-11-18 15:10:36 +0200

    Add setup method to UserApiClientTest for test initialization

--- Frontend/src/test/java/fronttest/UserApiClientTest.java
+++ Frontend/src/test/java/fronttest/UserApiClientTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import model.User;
@@ -16,6 +17,12 @@ class UserApiClientTest {
     private User user;
     private LoginRequest loginRequest;
     private UserApiClient userApiClient;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        userApiClient = new UserApiClient();
+    }
 
     @Test
     void postUserTest() {
