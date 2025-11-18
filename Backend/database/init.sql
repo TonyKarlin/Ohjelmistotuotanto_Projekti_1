435e649da85a9647d9cf31f09fc92f7d4aabef75
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2025-11-18 21:04:01 +0200

    fix: add missing comma in user table definition for maintainability

--- Backend/database/init.sql
+++ Backend/database/init.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS users
     password   VARCHAR(255) NOT NULL,
     email      VARCHAR(255) NOT NULL,
     created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    profile_picture VARCHAR(255)
+    profile_picture VARCHAR(255),
     language        VARCHAR(10)  DEFAULT 'en'
 );
 
