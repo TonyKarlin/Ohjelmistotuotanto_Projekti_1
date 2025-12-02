3bb920c45233e2e5e561ccf12a51e1f7f77f11e0
jonijermu <joni.heikkila@metropolia.fi>
2025-12-02 14:13:37 +0200

    fixed GlobalEventHandler removing old element when switching views

--- Frontend/src/main/java/utils/GlobalEventHandler.java
+++ Frontend/src/main/java/utils/GlobalEventHandler.java
@@ -1,5 +1,6 @@
 package utils;
 
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
@@ -12,13 +13,19 @@ public class GlobalEventHandler {
         // Private constructor to hide the implicit public one
     }
 
+    private static final EventHandler<KeyEvent> ENTER_HANDLER = event -> {
+        Node node = (Node) event.getSource();
+        Button btn = (Button) node.getProperties().get("enterButton");
+        if (event.getCode() == KeyCode.ENTER && btn != null) {
+            btn.fire();
+            event.consume();
+        }
+    };
+
     public static void setButtonEventHandler(Node element, Button btn) {
-        element.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
-            if (event.getCode() == KeyCode.ENTER) {
-                btn.fire();
-                event.consume();
-            }
-        });
+        element.getProperties().put("enterButton", btn);
+        element.removeEventHandler(KeyEvent.KEY_PRESSED, ENTER_HANDLER);
+        element.addEventHandler(KeyEvent.KEY_PRESSED, ENTER_HANDLER);
     }
 
     public static void addObservableEventListener(Node element, Button btn) {
